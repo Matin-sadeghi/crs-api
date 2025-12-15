36c448c1e3057634667cbc825956d725d444450f
matin sadeghi <matin.sadeghiiii@gmail.com>
2025-12-15 19:50:06 +0330

    feat: implement student CRUD operations
    - Added methods for creating, retrieving, updating, and deleting students in StudentController and StudentService.
    - Implemented role-based access control for student management using Roles and JwtAuthGuard.
    - Enhanced StudentRepository with methods for fetching all students and handling updates and deletions.
    - Introduced UpdateStudentDto for updating student details, including optional fields.
    - Enhanced documentation with API operation summaries for better clarity.

--- src/student/controller/student.controller.ts
+++ src/student/controller/student.controller.ts
@@ -1,12 +1,26 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
 import { ApiBearerAuth, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { HttpResponseDto } from 'src/utils/util.dto';
-import { CreateStudentDto } from '../dtos/student.dto';
+import {
+  CreateStudentDto,
+  StudentResponseDto,
+  UpdateStudentDto,
+} from '../dtos/student.dto';
 import { StudentService } from '../services/student.service';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { Roles } from 'src/auth/decorators/roles.decorator';
 import { UserRole } from 'src/utils/enum';
+import { StudentDocument } from '../database/schema/student.schema';
 
 @Controller('student')
 @ApiBearerAuth('JWT-auth')
@@ -22,5 +36,49 @@ export class StudentController {
   })
   create(@Body() dto: CreateStudentDto): Promise<HttpResponseDto> {
     return this.studentService.createUser(dto);
+  }
+
+  @Get()
+  @Roles(UserRole.ADMIN)
+  @ApiOperation({ summary: 'Get all students' })
+  @ApiResponse({
+    type: StudentResponseDto,
+    isArray: true,
+  })
+  getAllStudents(): Promise<StudentDocument[]> {
+    return this.studentService.getAllStudents();
+  }
+
+  @Get(':id')
+  @Roles(UserRole.ADMIN)
+  @ApiOperation({ summary: 'Get one student' })
+  @ApiResponse({
+    type: StudentResponseDto,
+  })
+  getStudentById(@Param('id') id: string): Promise<StudentDocument> {
+    return this.studentService.getStudentById(id);
+  }
+
+  @Delete(':id')
+  @Roles(UserRole.ADMIN)
+  @ApiOperation({ summary: 'Delete a student' })
+  @ApiResponse({
+    type: HttpResponseDto,
+  })
+  deleteStudent(@Param('id') id: string): Promise<HttpResponseDto> {
+    return this.studentService.deleteStudent(id);
+  }
+
+  @Put(':id')
+  @Roles(UserRole.ADMIN)
+  @ApiOperation({ summary: 'Update a student' })
+  @ApiResponse({
+    type: HttpResponseDto,
+  })
+  updateStudent(
+    @Param('id') id: string,
+    @Body() updateStudentDto: UpdateStudentDto,
+  ): Promise<HttpResponseDto> {
+    return this.studentService.updateStudent(id, updateStudentDto);
   }
 }
